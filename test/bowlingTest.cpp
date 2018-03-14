52208532787bf636c64096f626ab362ff0e5ad2e
Boguslaw Krawczuk <boguslaw@oc8227423143.ibm.com>
2018-03-14 23:27:25 +0100

    simplifications of tests

--- test/bowlingTest.cpp
+++ test/bowlingTest.cpp
@@ -5,80 +5,73 @@ using namespace std;
 
 class bowlingTest : public ::testing::Test
 {
-
+public:
+  Bowling game;
 };
 
 
 TEST_F(bowlingTest, emptyToken)
 {
-  Bowling game;
   game.fillTokens("");  
   auto tokens = game.getTokens();
   ASSERT_FALSE(tokens.empty());
-  ASSERT_TRUE(tokens.at(0) == "");
-// the question is: should really Tokens be empty? Or maybe they should have one empty string instead...
+  ASSERT_EQ(tokens.at(0), "");
 }
 
 
 TEST_F(bowlingTest, oneSimpleToken)
 {
-  Bowling game;
   string s = "a";
   game.fillTokens(s);  
   auto tokens = game.getTokens();
   ASSERT_FALSE(tokens.empty());
-  ASSERT_TRUE(tokens.at(0) == s);
+  ASSERT_EQ(tokens.at(0), s);
 }
 
 
 TEST_F(bowlingTest, twoEmptyTokens)
 {
-  Bowling game;
   game.fillTokens("|");  
   auto tokens = game.getTokens();
   ASSERT_FALSE(tokens.empty());
-  ASSERT_TRUE(tokens.at(0) == "");
-  ASSERT_TRUE(tokens.at(1) == "");
+  ASSERT_EQ(tokens.at(0), "");
+  ASSERT_EQ(tokens.at(1), "");
 }
 
 TEST_F(bowlingTest, threeTokens)
 {
-  Bowling game;
   game.fillTokens("a|grrr|X");  
   auto tokens = game.getTokens();
-  ASSERT_TRUE(tokens.at(0) == "a");
-  ASSERT_TRUE(tokens.at(1) == "grrr");
-  ASSERT_TRUE(tokens.at(2) == "X");
+  ASSERT_EQ(tokens.at(0), "a");
+  ASSERT_EQ(tokens.at(1), "grrr");
+  ASSERT_EQ(tokens.at(2), "X");
 }
 
 
 TEST_F(bowlingTest, fourTokens)
 {
-  Bowling game;
   game.fillTokens("a||X|123456");  
   auto tokens = game.getTokens();
-  ASSERT_TRUE(tokens.at(0) == "a");
-  ASSERT_TRUE(tokens.at(1) == "");
-  ASSERT_TRUE(tokens.at(2) == "X");
-  ASSERT_TRUE(tokens.at(3) == "123456");
+  ASSERT_EQ(tokens.at(0), "a");
+  ASSERT_EQ(tokens.at(1), "");
+  ASSERT_EQ(tokens.at(2), "X");
+  ASSERT_EQ(tokens.at(3), "123456");
 }
 
 TEST_F(bowlingTest, allTokens)
 {
-  Bowling game;
   game.fillTokens("X|7/|9-|X|-8|8/|-6|X|X|X||81");  
   auto tokens = game.getTokens();
-  ASSERT_TRUE(tokens.at(0) == "X");
-  ASSERT_TRUE(tokens.at(1) == "7/");
-  ASSERT_TRUE(tokens.at(2) == "9-");
-  ASSERT_TRUE(tokens.at(3) == "X");
-  ASSERT_TRUE(tokens.at(9) == "X");
-  ASSERT_TRUE(tokens.at(10) == "81");
+  ASSERT_EQ(tokens.at(0), "X");
+  ASSERT_EQ(tokens.at(1), "7/");
+  ASSERT_EQ(tokens.at(2), "9-");
+  ASSERT_EQ(tokens.at(3), "X");
+  ASSERT_EQ(tokens.at(9), "X");
+  ASSERT_EQ(tokens.at(10), "81");
 }
 
 TEST_F(bowlingTest, validation)
 {
-    Bowling game;
     game.fillTokens("X|7/|9-|X|-8|8/|-6|X|X|X||81");
     auto tokens = game.getTokens();
     ASSERT_TRUE(game.validateIsCorrect(tokens));
